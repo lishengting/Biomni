e2c5fe4b5de948400642676f4457eae602f84877
lishengting <lishengting@gmail.com>
2025-07-17 15:19:33 +0800

    更新llm.py，调整阿里云Qwen模型的参数处理方式，将extra_body中的chat_template_kwargs参数替换为enable_thinking参数以优化模型配置。旧代码已注释以供参考。

--- biomni/llm.py
+++ biomni/llm.py
@@ -107,7 +107,8 @@ def get_llm(
         if is_aliyun_qwen:
             #kwargs["chat_template_kwargs"] = {"enable_thinking": False}
             #kwargs["enable_thinking"] = False
-            kwargs["extra_body"] = {"chat_template_kwargs": {"enable_thinking": False}}
+            #kwargs["extra_body"] = {"chat_template_kwargs": {"enable_thinking": False}}
+            kwargs["extra_body"] = {"enable_thinking": False}
         
         return ChatOpenAI(**kwargs)
     else:
